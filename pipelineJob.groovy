de69d521f0a89254751dfd4225361288d8fe6ee0
nci-snow-cicd <61884139+nci-snow-cicd@users.noreply.github.com>
2020-09-08 16:31:27 -0400

    Update pipelineJob.groovy

--- pipelineJob.groovy
+++ pipelineJob.groovy
@@ -13,17 +13,3 @@ pipeline {
         }
     }
 }
-pipelineJob('theme-park-job') {
-    definition {
-        cpsScm {
-            scm {
-                git {
-                    remote {
-                        url 'https://github.com/tkgregory/spring-boot-api-example.git'
-                    }
-                    branch 'master'
-                }
-            }
-        }
-    }
-}
