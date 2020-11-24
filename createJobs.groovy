9b02ac33af13ef37fd2b45c8921d34b4fdbe3f44
nci-snow-cicd <61884139+nci-snow-cicd@users.noreply.github.com>
2020-11-24 11:29:33 -0500

    Update createJobs.groovy

--- createJobs.groovy
+++ createJobs.groovy
@@ -194,6 +194,40 @@ pipelineJob('alpine-job-docker') {
         }
     }
 }
+pipelineJob('centos-job-docker') {
+     properties {
+        office365ConnectorWebhooks {
+            webhooks {
+                webhook {
+                    name('Teams Webhook')
+                    url('https://outlook.office.com/webhook/6faa453a-84bf-4bfb-8a01-c0373d260d02@a5c2ab90-5b50-4606-9f1d-83c879f0a49f/IncomingWebhook/2bb9efbe59534841bc7f4ba1f8f787dd/4088b722-d5d7-4c04-b7af-4b2a5ea227cb')
+                    startNotification(true)
+                    notifySuccess(true)
+                    notifyAborted(true)
+                    notifyNotBuilt(true)
+                    notifyUnstable(true)
+                    notifyFailure(true)
+                    notifyBackToNormal(false)
+                    notifyRepeatedFailure(false)
+                    timeout(30000)
+                }
+            }
+        }
+    }
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/nci-snow-cicd/centos.git'
+                    }
+                    branch 'master'
+                    scriptPath('centos-cbiit-docker')
+                }
+            }
+        }
+    }
+}
 pipelineJob('sonarcube-job-docker') {
      properties {
         office365ConnectorWebhooks {
